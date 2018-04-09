a2c3b76b3c193a435a8bfd87cfe14b87f0a76cc1
ligulfzhou <ligulfzhou53@gmail.com>
2018-04-09 09:48:02 +0800

    change random num generate policy

--- scrap_bitcoins.py
+++ scrap_bitcoins.py
@@ -22,10 +22,9 @@ class Bitcoin:
 
     def __init__(self, loop):
         self.loop = loop
-        self.electrumx_host = '69.51.11.34'
+        self.electrumx_host = '127.0.0.1'
         self.electrumx_port = 50001
         self.leveldb_host = '127.0.0.1'
-        self.electrumx_host = self.leveldb_host
         self.leveldb_port = 50005
 
     async def get_bitcoin_balance(self, addr):
@@ -49,6 +48,8 @@ class Bitcoin:
         return res
 
     async def _send_addr_priv(self, pairs):
+        # 原先有将生成的私钥和地址入leveldb的,以地址为k,私钥为v
+        # 获取到交易通知时，按交易的发送方和接收方的地址去查这个leveldb去获得私钥
         reader, writer = await asyncio.open_connection(self.leveldb_host, self.leveldb_port, loop=self.loop)
         data = {
             'params': [pairs],
@@ -94,7 +95,10 @@ class Bitcoin:
 
     async def start_scrap(self):
         while True:
-            num = random.randint(1, 115792089237316195423570985008687907853269984665640564039457584007913129639937)
+            tmp = random.randint(1, 115792089237316195423570985008687907853269984665640564039457584007913129639937)
+            num = tmp // (10**random.randint(0, len(str(tmp))))
+            if not num:
+                num = tmp
             logger.info('No: {}'.format(num))
             prikeys = await self.get_has_balance_prikey(num)
             if prikeys:
